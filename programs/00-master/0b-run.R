78beff2c82340869e115dd2f5a026d085b27a26e
souleymanefaye <souleymane.faye@ens.psl.eu>
2025-05-26 21:30:31 +0200

    update report

--- programs/00-master/0b-run.R
+++ programs/00-master/0b-run.R
@@ -9,3 +9,12 @@
 dir.create(file.path(root, "figures"), showWarnings = FALSE)
 dir.create(file.path(root, "tables"), showWarnings = FALSE)
 dir.create(file.path(root, "work-data"), showWarnings = FALSE)
+
+# Import and clean dataset
+source(file.path(root,"programs/01-import.R"))
+
+# Univariate analysis
+source(file.path(root,"programs/02-univariate-analysis.R"))
+
+# Multivariate analysis
+source(file.path(root,"programs/03-multivariate-analysis.R"))
